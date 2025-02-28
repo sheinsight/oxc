14695723b7c7f73ebd1264d097ab7a679bf56610
ityuany <519495771@qq.com>
2025-03-01 00:51:06 +0800

    feat(linter): 添加和调整自定义规则
    扩展了多个 ESLint 规则支持，包括 EslintNoUnreachable, EslintNoUnsafeFinally 等。加入了新的导入和 React 相关的规则，并且调整了部分现有规则的配置以提升代码质量和一致性。

--- crates/oxc_linter/src/custom/mod.rs
+++ crates/oxc_linter/src/custom/mod.rs
@@ -31,7 +31,33 @@ use crate::{
         EslintNoSetterReturn, EslintNoShadowRestrictedNames, EslintNoSpacedFunc,
         EslintNoSparseArrays, EslintNoTemplateCurlyInString, EslintNoTernary,
         EslintNoThisBeforeSuper, EslintNoThrowLiteral, EslintNoUndef, EslintNoUndefined,
-        EslintNoUnexpectedMultiline, EslintNoUnneededTernary, EslintNoUnusedVars, RuleEnum,
+        EslintNoUnexpectedMultiline, EslintNoUnneededTernary, EslintNoUnreachable,
+        EslintNoUnsafeFinally, EslintNoUnsafeNegation, EslintNoUnsafeOptionalChaining,
+        EslintNoUnusedExpressions, EslintNoUnusedLabels, EslintNoUnusedPrivateClassMembers,
+        EslintNoUnusedVars, EslintNoUselessCall, EslintNoUselessCatch, EslintNoUselessConcat,
+        EslintNoUselessConstructor, EslintNoUselessEscape, EslintNoUselessRename, EslintNoVar,
+        EslintNoVoid, EslintNoWith, EslintOperatorAssignment, EslintPreferExponentiationOperator,
+        EslintPreferNumericLiterals, EslintPreferObjectHasOwn, EslintPreferObjectSpread,
+        EslintPreferPromiseRejectErrors, EslintPreferRestParams, EslintPreferSpread, EslintRadix,
+        EslintRequireAwait, EslintRequireYield, EslintSortImports, EslintSortKeys, EslintSortVars,
+        EslintSymbolDescription, EslintUnicodeBom, EslintUseIsnan, EslintValidTypeof,
+        EslintVarsOnTop, EslintYoda, ImportDefault, ImportExport, ImportFirst,
+        ImportMaxDependencies, ImportNamed, ImportNamespace, ImportNoAbsolutePath, ImportNoAmd,
+        ImportNoCommonjs, ImportNoCycle, ImportNoDefaultExport, ImportNoDuplicates,
+        ImportNoDynamicRequire, ImportNoMutableExports, ImportNoNamedAsDefault,
+        ImportNoNamedAsDefaultMember, ImportNoNamedDefault, ImportNoNamespace, ImportNoSelfImport,
+        ImportNoWebpackLoaderSyntax, ImportUnambiguous, ReactButtonHasType,
+        ReactCheckedRequiresOnchangeOrReadonly, ReactExhaustiveDeps, ReactIframeMissingSandbox,
+        ReactJsxBooleanValue, ReactJsxCurlyBracePresence, ReactJsxKey, ReactJsxNoCommentTextnodes,
+        ReactJsxNoDuplicateProps, ReactJsxNoScriptUrl, ReactJsxNoTargetBlank, ReactJsxNoUndef,
+        ReactJsxNoUselessFragment, ReactJsxPropsNoSpreadMulti, ReactNoArrayIndexKey,
+        ReactNoChildrenProp, ReactNoDanger, ReactNoDangerWithChildren, ReactNoDirectMutationState,
+        ReactNoFindDomNode, ReactNoIsMounted, ReactNoRenderReturnValue, ReactNoSetState,
+        ReactNoStringRefs, ReactNoUnescapedEntities, ReactNoUnknownProperty,
+        ReactPerfJsxNoJsxAsProp, ReactPerfJsxNoNewArrayAsProp, ReactPerfJsxNoNewFunctionAsProp,
+        ReactPerfJsxNoNewObjectAsProp, ReactPreferEs6Class, ReactReactInJsxScope,
+        ReactRequireRenderReturn, ReactRulesOfHooks, ReactSelfClosingComp, ReactStylePropObject,
+        ReactVoidDomElementsNoChildren, RuleEnum,
     },
 };
 
@@ -74,7 +100,14 @@ impl CustomLinter {
         let severity_by_mode = if self.mode == LintMode::Production { 2 } else { 1 };
 
         vec![
-            c!(2, EslintArrayCallbackReturn, json!({"checkForEach": true, "allowImplicit": false})),
+            c!(
+                2,
+                EslintArrayCallbackReturn,
+                json!({
+                    "checkForEach": true,
+                    "allowImplicit": false
+                })
+            ),
             // TODO: prefer 2
             c!(
                 1,
@@ -96,30 +129,67 @@ impl CustomLinter {
             c!(2, EslintCurly, json!(["all"])),
             c!(2, EslintDefaultCaseLast),
             c!(2, EslintDefaultParamLast),
-            c!(2, EslintEqeqeq, json!(["always",{"null":"always"}])),
+            c!(
+                2,
+                EslintEqeqeq,
+                json!(["always",{
+                    "null":"always"
+                }])
+            ),
             c!(2, EslintForDirection),
             c!(2, EslintFuncNames, json!(["as-needed"])),
-            c!(2, EslintFuncStyle, json!(["declaration", { "allowArrowFunctions": true }])),
-            c!(2, EslintGetterReturn, json!(["always", { "allowImplicit": false }])),
+            c!(
+                2,
+                EslintFuncStyle,
+                json!(["declaration", {
+                    "allowArrowFunctions": true
+                }])
+            ),
+            c!(
+                2,
+                EslintGetterReturn,
+                json!(["always", {
+                "allowImplicit": false
+                }])
+            ),
             c!(2, EslintGroupedAccessorPairs, json!(["anyOrder"])),
             c!(2, EslintGuardForIn),
             // TODO: 理论上应该检测循环内必须初始化，其他的不管，但是好像无法这样配置 , 所以关闭
-            c!(0, EslintInitDeclarations, json!(["always", { "ignoreForLoopInit": true }])),
+            c!(
+                0,
+                EslintInitDeclarations,
+                json!(["always", {
+                   "ignoreForLoopInit": true
+                }])
+            ),
             // TODO: 限制一个文件只能有一个 class , 我个人认为是合理的。
-            c!(2, EslintMaxClassesPerFile, json!(["1", { "ignoreExpressions": false }])),
+            c!(
+                2,
+                EslintMaxClassesPerFile,
+                json!(["1", {
+                    "ignoreExpressions": false
+                }])
+            ),
             // TODO: 代码嵌套层数，实际代码中还是很难做到的，函数抽多了名字也不好起，先关闭，后续再说
             c!(0, EslintMaxDepth, json!(["10"])),
             // TODO: 实际代码中还是很难做到的，函数抽多了名字也不好起，先关闭，后续再说
             c!(
                 0,
                 EslintMaxLinesPerFunction,
-                json!(["100", { "skipComments": true, "skipBlankLines": true, "iifes": true }])
+                json!(["100", {
+                    "skipComments": true,
+                    "skipBlankLines": true,
+                    "iifes": true
+                }])
             ),
             // TODO: 这个我觉得实际工作中还是比较容易做到 1000 行以内的。
             c!(
                 2,
                 EslintMaxLines,
-                json!(["1000", { "skipBlankLines": true, "skipComments": true }])
+                json!(["1000", {
+                    "skipBlankLines": true,
+                    "skipComments": true
+                }])
             ),
             // TODO: 这个我觉得实际工作中还是比较容易做到 cb 嵌套控制在 10 以内的
             c!(2, EslintMaxNestedCallbacks, json!(["10"])),
@@ -142,7 +212,13 @@ impl CustomLinter {
             c!(2, EslintNoConstAssign),
             c!(2, EslintNoConstantBinaryExpression),
             // TODO: 循环内不检查
-            c!(2, EslintNoConstantCondition, json!({"checkLoops":false})),
+            c!(
+                2,
+                EslintNoConstantCondition,
+                json!({
+                    "checkLoops":false
+                })
+            ),
             c!(2, EslintNoConstructorReturn),
             c!(0, EslintNoContinue),
             c!(2, EslintNoControlRegex),
@@ -154,26 +230,59 @@ impl CustomLinter {
             c!(2, EslintNoDupeKeys),
             c!(2, EslintNoDuplicateCase),
             // TODO: 有可能跟 ts 的 import type 冲突，后续需要验证
-            c!(2, EslintNoDuplicateImports, json!({"includeExports":true})),
-            c!(0, EslintNoElseReturn, json!({"allowElseIf":true})),
+            c!(
+                2,
+                EslintNoDuplicateImports,
+                json!({
+                    "includeExports":true
+                })
+            ),
+            c!(
+                0,
+                EslintNoElseReturn,
+                json!({
+                    "allowElseIf":true
+                })
+            ),
             c!(2, EslintNoEmptyCharacterClass),
             c!(severity_by_mode, EslintNoEmptyFunction),
             c!(severity_by_mode, EslintNoEmptyPattern),
             c!(severity_by_mode, EslintNoEmptyStaticBlock),
-            c!(severity_by_mode, EslintNoEmpty, json!({"allowEmptyCatch":true})),
+            c!(
+                severity_by_mode,
+                EslintNoEmpty,
+                json!({
+                    "allowEmptyCatch":true
+                })
+            ),
             c!(2, EslintNoEqNull),
             // 允许这种逃逸手段
             // const indirect = eval;
             // indirect('var x = 1');
-            c!(severity_by_mode, EslintNoEval, json!({"allowIndirect":true})),
+            c!(
+                severity_by_mode,
+                EslintNoEval,
+                json!({
+                    "allowIndirect":true
+                })
+            ),
             c!(2, EslintNoExAssign),
             c!(2, EslintNoExtendNative, json!({"exceptions":[]})),
-            c!(2, EslintNoExtraBooleanCast, json!({"enforceForLogicalOperands":true})),
+            c!(
+                2,
+                EslintNoExtraBooleanCast,
+                json!({
+                    "enforceForLogicalOperands":true
+                })
+            ),
             c!(2, EslintNoExtraLabel),
             c!(
                 2,
                 EslintNoFallthrough,
-                json!({"allowEmptyCase":true, "reportUnusedFallthroughComment":true})
+                json!({
+                    "allowEmptyCase":true,
+                    "reportUnusedFallthroughComment":true
+                })
             ),
             c!(2, EslintNoFuncAssign),
             c!(2, EslintNoGlobalAssign, json!({"exceptions":[]})),
@@ -225,7 +334,227 @@ impl CustomLinter {
             c!(2, EslintNoUndef),
             c!(2, EslintNoUndefined),
             c!(2, EslintNoUnexpectedMultiline),
-            c!(1, EslintNoUnneededTernary),
+            c!(1, EslintNoUnneededTernary, json!({ "defaultAssignment": true })),
+            c!(2, EslintNoUnreachable),
+            c!(2, EslintNoUnsafeFinally),
+            c!(2, EslintNoUnsafeNegation, json!({ "enforceForOrderingRelations": true })),
+            c!(2, EslintNoUnsafeOptionalChaining, json!({ "disallowArithmeticOperators": false })),
+            c!(
+                1,
+                EslintNoUnusedExpressions,
+                json!({
+                    "allowShortCircuit": true,
+                    "allowTernary": true,
+                    "allowTaggedTemplates": true,
+                    "enforceForJSX": false
+                })
+            ),
+            c!(2, EslintNoUnusedLabels),
+            c!(1, EslintNoUnusedPrivateClassMembers),
+            c!(1, EslintNoUselessCall),
+            c!(1, EslintNoUselessCatch),
+            c!(1, EslintNoUselessConcat),
+            c!(1, EslintNoUselessConstructor),
+            c!(1, EslintNoUselessEscape),
+            c!(1, EslintNoUselessRename),
+            c!(2, EslintNoVar),
+            c!(0, EslintNoVoid, json!({ "allowAsStatement": true })),
+            // TODO 微前端需要
+            c!(1, EslintNoWith),
+            c!(1, EslintOperatorAssignment, json!({ "mode": "always" })),
+            c!(1, EslintPreferExponentiationOperator),
+            c!(1, EslintPreferNumericLiterals),
+            c!(2, EslintPreferObjectHasOwn),
+            c!(2, EslintPreferObjectSpread),
+            c!(2, EslintPreferPromiseRejectErrors, json!({"allowThenable":true})),
+            c!(2, EslintPreferRestParams),
+            c!(2, EslintPreferSpread),
+            c!(0, EslintRadix),
+            c!(2, EslintRequireAwait),
+            c!(2, EslintRequireYield),
+            c!(0, EslintSortImports),
+            c!(0, EslintSortKeys),
+            c!(0, EslintSortVars),
+            c!(2, EslintSymbolDescription),
+            c!(0, EslintUnicodeBom),
+            c!(2, EslintUseIsnan),
+            c!(2, EslintValidTypeof, json!({ "requireStringLiterals": true })),
+            c!(2, EslintVarsOnTop),
+            c!(0, EslintYoda, json!({ "never": true })),
+        ]
+    }
+
+    // 所有路径相关的都是通过 oxc_resolver 去解析 tsconfig.json 的 path 来实现的
+    fn get_import_rules(&self) -> Vec<RuleWithSeverity> {
+        vec![
+            c!(2, ImportDefault),
+            c!(2, ImportExport),
+            c!(
+                2,
+                ImportFirst,
+                json!({
+                    "absoluteFirst": false
+                })
+            ),
+            c!(
+                0,
+                ImportMaxDependencies,
+                json!({
+                    "max": 10
+                })
+            ),
+            c!(2, ImportNamed),
+            c!(
+                2,
+                ImportNamespace,
+                json!({
+                    "allowComputed": false
+                })
+            ),
+            c!(
+                2,
+                ImportNoAbsolutePath,
+                json!({
+                    "esmodule": true,
+                    "commonjs": true,
+                    "amd": true
+                })
+            ),
+            c!(0, ImportNoAmd),
+            c!(0, ImportNoCommonjs),
+            c!(
+                2,
+                ImportNoCycle,
+                json!({
+                    "maxDepth": 3,
+                    "ignoreTypes": true,
+                    "ignoreExternal": true,
+                    "allowUnsafeDynamicCyclicDependency": false
+                })
+            ),
+            c!(0, ImportNoDefaultExport),
+            // TODO 跟 EslintNoDuplicateImports 有点重复
+            c!(0, ImportNoDuplicates),
+            // TODO 这个确实不好，但是限制不住，很多场景可能确实需要
+            c!(0, ImportNoDynamicRequire),
+            c!(2, ImportNoMutableExports),
+            c!(2, ImportNoNamedAsDefaultMember),
+            c!(2, ImportNoNamedAsDefault),
+            c!(2, ImportNoNamedDefault),
+            c!(1, ImportNoNamespace),
+            c!(2, ImportNoSelfImport),
+            c!(2, ImportNoWebpackLoaderSyntax),
+            // TODO 赞同这个规范，但是总感觉有些场景存在这个必要性，所以先设置警告
+            c!(1, ImportUnambiguous),
+        ]
+    }
+
+    fn get_react_rules(&self) -> Vec<RuleWithSeverity> {
+        vec![
+            c!(
+                2,
+                ReactButtonHasType,
+                json!({
+                    "button": true,
+                    "submit": true,
+                    "reset": true
+                })
+            ),
+            c!(
+                2,
+                ReactCheckedRequiresOnchangeOrReadonly,
+                json!({
+                    "ignore_missing_properties": true,
+                    "ignore_exclusive_checked_attribute": true
+                })
+            ),
+            // TODO 过于教条，其实有些场景内部依赖的值就是不会变化的。先警告
+            c!(1, ReactExhaustiveDeps),
+            c!(2, ReactIframeMissingSandbox),
+            c!(
+                2,
+                ReactJsxBooleanValue,
+                json!(["never",{
+                    "exceptions":[],
+                    "assumeUndefinedIsFalse":true
+                }])
+            ),
+            c!(
+                1,
+                ReactJsxCurlyBracePresence,
+                json!({
+                    "props": "always",
+                    "children": "always",
+                    "propElementValues": "always"
+                })
+            ),
+            c!(2, ReactJsxKey),
+            c!(2, ReactJsxNoCommentTextnodes),
+            c!(2, ReactJsxNoDuplicateProps),
+            c!(0, ReactJsxNoScriptUrl),
+            c!(0, ReactJsxNoTargetBlank),
+            c!(0, ReactJsxNoUndef),
+            c!(0, ReactJsxNoUselessFragment),
+            c!(2, ReactJsxPropsNoSpreadMulti),
+            // TODO 规则是对的，实际工作中很难落地
+            c!(1, ReactNoArrayIndexKey),
+            c!(2, ReactNoChildrenProp),
+            c!(2, ReactNoDangerWithChildren),
+            // TODO 有些时候还是需要注入 html 的
+            c!(0, ReactNoDanger),
+            c!(2, ReactNoDirectMutationState),
+            c!(1, ReactNoFindDomNode),
+            c!(1, ReactNoIsMounted),
+            c!(2, ReactNoRenderReturnValue),
+            c!(2, ReactNoSetState),
+            c!(
+                2,
+                ReactNoStringRefs,
+                json!({
+                    "noTemplateLiterals":true
+                })
+            ),
+            c!(1, ReactNoUnescapedEntities),
+            // 感觉官方代码还没写完，所以先关闭
+            c!(
+                0,
+                ReactNoUnknownProperty,
+                json!({
+                    "ignore":[]
+                })
+            ),
+            c!(0, ReactPreferEs6Class),
+            // TODO 要跟 jsx-runtime 联动
+            c!(2, ReactReactInJsxScope),
+            c!(2, ReactRequireRenderReturn),
+            c!(1, ReactRulesOfHooks),
+            c!(
+                1,
+                ReactSelfClosingComp,
+                json!({
+                    "component":true,
+                    "html":true
+                })
+            ),
+            c!(
+                2,
+                ReactStylePropObject,
+                json!({
+                    "allow":[]
+                })
+            ),
+            c!(2, ReactVoidDomElementsNoChildren),
+        ]
+    }
+
+    fn get_react_perf_rules(&self) -> Vec<RuleWithSeverity> {
+        vec![
+            // 实际工作中几乎不可能满足这个要求
+            c!(0, ReactPerfJsxNoJsxAsProp),
+            c!(1, ReactPerfJsxNoNewArrayAsProp),
+            // 可能要考虑未来跟 react compile 联动
+            c!(1, ReactPerfJsxNoNewFunctionAsProp),
+            c!(1, ReactPerfJsxNoNewObjectAsProp),
         ]
     }
 
