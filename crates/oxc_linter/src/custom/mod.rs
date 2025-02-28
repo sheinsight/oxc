9ec375ab91c27a7d274a6ac3cdf9ae16760c1121
ityuany <519495771@qq.com>
2025-02-28 20:03:22 +0800

    feat(linter): 增加并配置多条 ESLint 规则
    增加了多个 ESLint 规则，包括 no-console、no-constant-condition、no-dupe-keys 等。此外，根据不同的模式设置了部分规则的严重级别。

--- crates/oxc_linter/src/custom/mod.rs
+++ crates/oxc_linter/src/custom/mod.rs
@@ -11,8 +11,27 @@ use crate::{
         EslintMaxLines, EslintMaxLinesPerFunction, EslintMaxNestedCallbacks, EslintMaxParams,
         EslintNewCap, EslintNoAlert, EslintNoArrayConstructor, EslintNoAsyncPromiseExecutor,
         EslintNoAwaitInLoop, EslintNoBitwise, EslintNoCaller, EslintNoCaseDeclarations,
-        EslintNoClassAssign, EslintNoCompareNegZero, EslintNoCondAssign, EslintNoUnusedVars,
-        RuleEnum,
+        EslintNoClassAssign, EslintNoCompareNegZero, EslintNoCondAssign, EslintNoConsole,
+        EslintNoConstAssign, EslintNoConstantBinaryExpression, EslintNoConstantCondition,
+        EslintNoConstructorReturn, EslintNoContinue, EslintNoControlRegex, EslintNoDebugger,
+        EslintNoDeleteVar, EslintNoDivRegex, EslintNoDupeClassMembers, EslintNoDupeElseIf,
+        EslintNoDupeKeys, EslintNoDuplicateCase, EslintNoDuplicateImports, EslintNoElseReturn,
+        EslintNoEmpty, EslintNoEmptyCharacterClass, EslintNoEmptyFunction, EslintNoEmptyPattern,
+        EslintNoEmptyStaticBlock, EslintNoEqNull, EslintNoEval, EslintNoExAssign,
+        EslintNoExtendNative, EslintNoExtraBooleanCast, EslintNoExtraLabel, EslintNoFallthrough,
+        EslintNoFuncAssign, EslintNoGlobalAssign, EslintNoImportAssign, EslintNoInnerDeclarations,
+        EslintNoInvalidRegexp, EslintNoIrregularWhitespace, EslintNoIterator, EslintNoLabelVar,
+        EslintNoLabels, EslintNoLoneBlocks, EslintNoLossOfPrecision, EslintNoMagicNumbers,
+        EslintNoMultiAssign, EslintNoMultiStr, EslintNoNegatedCondition, EslintNoNestedTernary,
+        EslintNoNew, EslintNoNewFunc, EslintNoNewNativeNonconstructor, EslintNoNewWrappers,
+        EslintNoNonoctalDecimalEscape, EslintNoObjCalls, EslintNoObjectConstructor,
+        EslintNoPlusplus, EslintNoProto, EslintNoPrototypeBuiltins, EslintNoRedeclare,
+        EslintNoRegexSpaces, EslintNoRestrictedGlobals, EslintNoRestrictedImports,
+        EslintNoReturnAssign, EslintNoScriptUrl, EslintNoSelfAssign, EslintNoSelfCompare,
+        EslintNoSetterReturn, EslintNoShadowRestrictedNames, EslintNoSpacedFunc,
+        EslintNoSparseArrays, EslintNoTemplateCurlyInString, EslintNoTernary,
+        EslintNoThisBeforeSuper, EslintNoThrowLiteral, EslintNoUndef, EslintNoUndefined,
+        EslintNoUnexpectedMultiline, EslintNoUnneededTernary, EslintNoUnusedVars, RuleEnum,
     },
 };
 
@@ -52,6 +71,8 @@ impl CustomLinter {
     }
 
     fn get_eslint_rules(&self) -> Vec<RuleWithSeverity> {
+        let severity_by_mode = if self.mode == LintMode::Production { 2 } else { 1 };
+
         vec![
             c!(2, EslintArrayCallbackReturn, json!({"checkForEach": true, "allowImplicit": false})),
             // TODO: prefer 2
@@ -105,7 +126,7 @@ impl CustomLinter {
             c!(2, EslintMaxParams, json!(["5"])),
             // TODO: 比较难做到，先关闭
             c!(0, EslintNewCap),
-            c!(if self.mode == LintMode::Production { 2 } else { 1 }, EslintNoAlert),
+            c!(severity_by_mode, EslintNoAlert),
             c!(2, EslintNoArrayConstructor),
             c!(2, EslintNoAsyncPromiseExecutor),
             // TODO: 这里为什么是 1 ? 因为 js 的某些并发控制可能需要在循环内 await Promise.race()
@@ -117,6 +138,94 @@ impl CustomLinter {
             c!(2, EslintNoClassAssign),
             c!(2, EslintNoCompareNegZero),
             c!(2, EslintNoCondAssign, json!(["except-parens"])),
+            c!(severity_by_mode, EslintNoConsole, json!({ "allow": ["info", "warn", "error"] })),
+            c!(2, EslintNoConstAssign),
+            c!(2, EslintNoConstantBinaryExpression),
+            // TODO: 循环内不检查
+            c!(2, EslintNoConstantCondition, json!({"checkLoops":false})),
+            c!(2, EslintNoConstructorReturn),
+            c!(0, EslintNoContinue),
+            c!(2, EslintNoControlRegex),
+            c!(severity_by_mode, EslintNoDebugger),
+            c!(2, EslintNoDeleteVar),
+            c!(2, EslintNoDivRegex),
+            c!(2, EslintNoDupeClassMembers),
+            c!(2, EslintNoDupeElseIf),
+            c!(2, EslintNoDupeKeys),
+            c!(2, EslintNoDuplicateCase),
+            // TODO: 有可能跟 ts 的 import type 冲突，后续需要验证
+            c!(2, EslintNoDuplicateImports, json!({"includeExports":true})),
+            c!(0, EslintNoElseReturn, json!({"allowElseIf":true})),
+            c!(2, EslintNoEmptyCharacterClass),
+            c!(severity_by_mode, EslintNoEmptyFunction),
+            c!(severity_by_mode, EslintNoEmptyPattern),
+            c!(severity_by_mode, EslintNoEmptyStaticBlock),
+            c!(severity_by_mode, EslintNoEmpty, json!({"allowEmptyCatch":true})),
+            c!(2, EslintNoEqNull),
+            // 允许这种逃逸手段
+            // const indirect = eval;
+            // indirect('var x = 1');
+            c!(severity_by_mode, EslintNoEval, json!({"allowIndirect":true})),
+            c!(2, EslintNoExAssign),
+            c!(2, EslintNoExtendNative, json!({"exceptions":[]})),
+            c!(2, EslintNoExtraBooleanCast, json!({"enforceForLogicalOperands":true})),
+            c!(2, EslintNoExtraLabel),
+            c!(
+                2,
+                EslintNoFallthrough,
+                json!({"allowEmptyCase":true, "reportUnusedFallthroughComment":true})
+            ),
+            c!(2, EslintNoFuncAssign),
+            c!(2, EslintNoGlobalAssign, json!({"exceptions":[]})),
+            c!(2, EslintNoImportAssign),
+            c!(2, EslintNoInnerDeclarations, json!({"config":{"Functions":true}})),
+            c!(2, EslintNoInvalidRegexp, json!({"allowConstructorFlags":[]})),
+            c!(2, EslintNoIrregularWhitespace),
+            c!(2, EslintNoIterator),
+            c!(2, EslintNoLabelVar),
+            c!(0, EslintNoLabels, json!({"allowLoop":true, "allowSwitch":true})),
+            c!(2, EslintNoLoneBlocks),
+            c!(2, EslintNoLossOfPrecision),
+            // TODO: 这个规则的配置项太多了，先关闭
+            c!(0, EslintNoMagicNumbers),
+            c!(2, EslintNoMultiAssign),
+            c!(2, EslintNoMultiStr),
+            c!(2, EslintNoNegatedCondition),
+            // TODO: 感觉完全禁止嵌套三元表达式也不太现实
+            c!(1, EslintNoNestedTernary),
+            // TODO 微前端需要
+            c!(1, EslintNoNewFunc),
+            c!(2, EslintNoNewNativeNonconstructor),
+            c!(2, EslintNoNewWrappers),
+            c!(2, EslintNoNew),
+            c!(2, EslintNoNonoctalDecimalEscape),
+            c!(2, EslintNoObjCalls),
+            c!(1, EslintNoObjectConstructor),
+            c!(0, EslintNoPlusplus),
+            c!(2, EslintNoProto),
+            c!(2, EslintNoPrototypeBuiltins),
+            c!(2, EslintNoRedeclare),
+            c!(2, EslintNoRegexSpaces),
+            c!(0, EslintNoRestrictedGlobals),
+            c!(0, EslintNoRestrictedImports),
+            c!(0, EslintNoReturnAssign),
+            // TODO javascript:void(0); 这个有些场景会用
+            c!(0, EslintNoScriptUrl),
+            c!(2, EslintNoSelfAssign),
+            c!(2, EslintNoSelfCompare),
+            c!(2, EslintNoSetterReturn),
+            c!(2, EslintNoShadowRestrictedNames),
+            c!(2, EslintNoSpacedFunc),
+            c!(2, EslintNoSparseArrays),
+            c!(1, EslintNoTemplateCurlyInString),
+            c!(0, EslintNoTernary),
+            c!(2, EslintNoThisBeforeSuper),
+            c!(2, EslintNoThrowLiteral),
+            // TODO 应该利用全局的 globals 配置 跟 define 联动配置
+            c!(2, EslintNoUndef),
+            c!(2, EslintNoUndefined),
+            c!(2, EslintNoUnexpectedMultiline),
+            c!(1, EslintNoUnneededTernary),
         ]
     }
 
